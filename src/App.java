7ab140089bfcf57b31b07aeb5010b0f1fd88ee20
Uriel Emiliano Galindo Lopez <ugalu448@gmail.com>
2023-10-16 14:51:53 -0600

    add package basics to start work

---
+++ src/App.java
@@ -0,0 +1,12 @@
+import utils.ui.MenuUi;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        MenuUi menuUi = new MenuUi();
+
+        menuUi.mostrarMenuOpciones();
+
+
+
+    }
+}
---
+++ src/utils/ui/MenuUi.java
@@ -0,0 +1,11 @@
+package utils.ui;
+
+public class MenuUi {
+  
+  public void mostrarMenuOpciones(){
+    System.out.println("-------Seleciona el tipo de que deseas seguir----------");
+    System.out.println("1. LIFO\n2. FIFO\n3. SFJ\n4.LFJ\n0. Salir");
+    System.out.print("Ingresa tu opción(0-4): ");
+  }
+
+}
