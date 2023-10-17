40c211ef2749bcc6f6389bdd277553b562917c7e
Uriel Emiliano Galindo Lopez <ugalu448@gmail.com>
2023-10-17 10:27:44 -0600

    feat(cola): Implements class queue

--- src/estructuras/Cola.java
+++ src/estructuras/Cola.java
@@ -1,5 +1,40 @@
 package estructuras;
 
+import nodos.Proceso;
+
 public class Cola {
-  
+  private Proceso inicio;
+
+  public void in( Proceso sig ){
+
+    if( inicio == null ) inicio = sig;
+    else{
+      Proceso aux = buscarPosicion();
+      aux.setProcesoSig(sig);
+    }
+  }
+
+  private Proceso buscarPosicion(){
+    Proceso aux = inicio;
+
+    while( aux.getProcesoSig() != null ){
+      aux = aux.getProcesoSig();
+    }
+
+    return aux;
+  }
+
+  public Proceso out(){
+    if( inicio == null ) return null;
+    else{
+      Proceso aux = inicio;
+      inicio = inicio.getProcesoSig();
+      
+      return aux;
+    }
+  }
+
+  public Cola(){
+    inicio = null;
+  }
 }
